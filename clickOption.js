4f1d5fb6f401922197a0174c3e71d2495cca12f0
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-02-24 00:02:20 +0900

    Update clickOption.js

--- clickOption.js
+++ clickOption.js
@@ -2,13 +2,27 @@ const opTitle = document.querySelector(".group_option .select_op");
 const opList = document.querySelector(".group_option .op_list");
 
 //  리스트 열고 닫기
-opTitle.addEventListener("click", function () {
+function optionOpen() {
   opList.classList.toggle("on");
-});
+}
 
 // 리스트 선택 이벤트
-opList.addEventListener("click", function (event) {
+function optionSelect(event) {
   if (event.target.tagName !== "BUTTON") return false;
   opTitle.innerText = event.target.innerText;
   opList.classList.remove("on");
-});
+}
+
+// 텍스트 색 바꾸기
+function colorChange(event) {
+  event.target.style.color = "#fff";
+}
+
+function colorOriginal(event) {
+  event.target.style.color = "black";
+}
+
+opList.addEventListener("click", optionSelect);
+opTitle.addEventListener("click", optionOpen);
+opList.addEventListener("mouseover", colorChange);
+opList.addEventListener("mouseout", colorOriginal);
