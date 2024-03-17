12cbdb67864e7155eebfa561fce55401e75a7475
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-03-17 17:51:17 +0900

    change class name

--- clickOption.js
+++ clickOption.js
@@ -1,16 +1,16 @@
-const opTitle = document.querySelector(".group_option .select_op");
-const opList = document.querySelector(".group_option .op_list");
+const optionTitle = document.querySelector(".nav .nav-btn");
+const optionList = document.querySelector(".nav .ul");
 
 //  리스트 열고 닫기
 function optionOpen() {
-  opList.classList.toggle("on");
+  optionList.classList.toggle("on");
 }
 
 // 리스트 선택 이벤트
 function optionSelect(event) {
   if (event.target.tagName !== "BUTTON") return false;
-  opTitle.innerText = event.target.innerText;
-  opList.classList.remove("on");
+  optionTitle.innerText = event.target.innerText;
+  optionList.classList.remove("on");
 }
 
 // 텍스트 색 바꾸기
@@ -22,7 +22,8 @@ function colorOriginal(event) {
   event.target.style.color = "black";
 }
 
-opList.addEventListener("click", optionSelect);
-opTitle.addEventListener("click", optionOpen);
-opList.addEventListener("mouseover", colorChange);
-opList.addEventListener("mouseout", colorOriginal);
+optionList.addEventListener("click", optionSelect);
+optionTitle.addEventListener("click", optionOpen);
+
+optionList.addEventListener("mouseover", colorChange);
+optionList.addEventListener("mouseout", colorOriginal);
